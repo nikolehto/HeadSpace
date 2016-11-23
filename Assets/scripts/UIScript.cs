b361e9bc3d74b56579e5c731ff15ff1966580a0f
nikolehto <niko.lehto@mbnet.fi>
2016-11-23 14:33:17 +0200

    Add project

---
+++ Assets/scripts/UIScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIScript : MonoBehaviour {
+    Gyroscope m_gyroscope;
+
+    // Use this for initialization
+    void Start()
+    {
+        m_gyroscope = Input.gyro;
+        m_gyroscope.enabled = true;
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Gyroscope attitude : " + m_gyroscope.attitude);
+        GUILayout.Label("Gyroscope gravity : " + m_gyroscope.gravity);
+        GUILayout.Label("Gyroscope rotationRate : " + m_gyroscope.rotationRate);
+        GUILayout.Label("Gyroscope rotationRateUnbiased : " + m_gyroscope.rotationRateUnbiased);
+        GUILayout.Label("Gyroscope updateInterval : " + m_gyroscope.updateInterval);
+        GUILayout.Label("Gyroscope userAcceleration : " + m_gyroscope.userAcceleration);
+    }
+}
---
+++ Assets/sensorRotator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sensorRotator : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        Input.compensateSensors = true;
+        Input.gyro.enabled = true;
+    }
+
+    void FixedUpdate()
+    {
+        transform.Rotate(-Input.gyro.rotationRateUnbiased.x, -Input.gyro.rotationRateUnbiased.y, Input.gyro.rotationRateUnbiased.z);
+    }
+
+}
